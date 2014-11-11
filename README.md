3df94386aa1f1e6143c312ba57dcce4d3b192bc9
Thalisson <thalissondev@gmail.com>
2014-11-10 22:31:35 -0200

    Send new modification

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Reex-Prolog
 Prolog installing OSX
 	Homebrew: http://brew.sh/
 	Prolog: brew install swi-prolog
+	Command: swipl 
 
 By Thalisson Estrela
--- init.pl
+++ init.pl
@@ -1,2 +1,9 @@
-/* by Thalisson */
+start :- 
+	write('Enter expression: '), read(X), verify(X).
 
+verify(X) :- 
+	writef("%s", [X]),
+	count(X).
+
+count(X) :-
+	writef("%s", ["Counting variables..."]).
